2db31ae370c42d7941e36419175b45f6b46830b9
zzzzzuuuuu <agu0830@naver.com>
2024-04-07 00:25:54 +0900

    feat: 투두리스트 구현

---
+++ week1/todoList/index.js
@@ -0,0 +1,39 @@
+document.getElementById("todoInput").addEventListener("keypress", (e) => {
+  if(e.key === "Enter") {
+    addTodo();
+  }
+});
+
+const addTodo = () => {
+  const todoInput = document.getElementById("todoInput");
+  const todoList = document.getElementById("todoList")
+
+  if (todoInput.value.trim() !== "") { // trim() 함수는 문자열 앞뒤 공백 제거
+    const li = document.createElement("li");
+    li.textContent = todoInput.value;
+    li.className = "todoItem";
+
+    const completeBtn = document.createElement("button");
+    completeBtn.textContent = "완료";
+    completeBtn.addEventListener("click", () => {
+      const completedList = document.getElementById("completedList");
+      completedList.appendChild(li);
+      li.className = "completedItem";
+      console.log("완료버튼 클릭!");
+
+      // 완료 버튼을 삭제 버튼으로
+      li.removeChild(completeBtn);
+      const deleteBtn = document.createElement("button");
+      deleteBtn.textContent = "삭제";
+      deleteBtn.addEventListener("click", () => {
+        completedList.removeChild(li);
+        console.log("삭제버튼 클릭!");
+      });
+      li.appendChild(deleteBtn);
+    });
+    li.appendChild(completeBtn);
+    todoList.appendChild(li);
+
+    todoInput.value = "";
+  }
+}
