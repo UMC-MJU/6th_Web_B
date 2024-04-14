47797d87efd9a1aa8b3cf6aa6a1eb41406bd8909
zzzzzuuuuu <agu0830@naver.com>
2024-04-14 15:46:11 +0900

    fix: 모달 창 띄웠을 때 배경 전체 덮도록

--- 조이-안지유/modal/modal_react/src/App.jsx
+++ 조이-안지유/modal/modal_react/src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import Modal from "./components/Modal.jsx";
-import "./App.css";
 
 function App() {
   const [modal, setModal] = useState(false);
@@ -13,7 +12,7 @@ function App() {
       <h1>안녕하세요!</h1>
       <p>내용내용내용</p>
       <button onClick={openModal}>버튼 열기</button>
-      {modal && <Modal setModal={setModal} />}
+      {modal && <Modal setModal={setModal}/>}
     </>
   )
 }
--- 조이-안지유/modal/modal_react/src/components/Modal.jsx
+++ 조이-안지유/modal/modal_react/src/components/Modal.jsx
@@ -8,17 +8,15 @@ const Modal = (props) => {
     console.log("모달이 꺼짐");
   }
   return (
-    <>
-      <div className="modal_container">
-        <div className="modal">
-          <div className="modal_title">안녕하세요</div>
-          <p className="modal_text">모달 내용은 어쩌고 어쩌고..</p>
-          <div className="btn-container">
-            <button onClick={closeModal}>닫기</button>
-          </div>
+    <div className="modal_container">
+      <div className="modal">
+        <div className="modal_title">안녕하세요</div>
+        <p className="modal_text">모달 내용은 어쩌고 어쩌고..</p>
+        <div className="btn-container">
+          <button onClick={closeModal}>닫기</button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
