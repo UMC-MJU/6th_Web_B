51e8a0f16dc52ec14a7beacdab254160edcbe721
zzzzzuuuuu <agu0830@naver.com>
2024-04-14 15:20:02 +0900

    fix: 불필요한 주석 및 콘솔 삭제

--- 조이-안지유/movie/src/components/Movies.jsx
+++ 조이-안지유/movie/src/components/Movies.jsx
@@ -4,8 +4,6 @@ import MovieInfo from "./MovieInfo.jsx";
 
 const Movies = (props) => {
   const {original_title, overview, poster_path, vote_average} = props.data;
-  console.log(props.data);
-
   const [isHover, setIsHover] = useState(false);
   const openInfo = () => {
     setIsHover(true);
@@ -16,7 +14,6 @@ const Movies = (props) => {
 
   return (
     <div className="movie" onMouseEnter={openInfo} onMouseLeave={closeInfo}>
-      {/*  마우스커서를 위로 가져다댔을때 영화 상세설명이 뜨도록  */}
       {isHover && <MovieInfo title={original_title} info={overview}/>}
       <img className="moviePoster" src={`https://image.tmdb.org/t/p/w500/${poster_path}`}/>
       <div className="movieBasic">
