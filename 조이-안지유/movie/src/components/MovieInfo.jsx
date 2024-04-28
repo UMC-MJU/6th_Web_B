5458b7b4fc551b49ad4679bed8655a1d17688089
zzzzzuuuuu <agu0830@naver.com>
2024-04-28 13:48:04 +0900

    style: styled-components로 변환

--- 조이-안지유/movie/src/components/MovieInfo.jsx
+++ 조이-안지유/movie/src/components/MovieInfo.jsx
@@ -1,19 +1,49 @@
 import React from 'react';
 import "../styles/movieinfo.css";
+import styled from "styled-components";
 
 const MovieInfo = (props) => {
   const title = props.title;
   const overview = props.info;
   return (
-    <div className="movieInfoContainer">
-      <div className="title">
+    <MovieInfoContainer>
+      <Title>
         {title}
-      </div>
-      <div className="overview">
+      </Title>
+      <Overview>
         {overview}
-      </div>
-    </div>
+      </Overview>
+    </MovieInfoContainer>
   );
 };
 
 export default MovieInfo;
+
+const MovieInfoContainer = styled.div`
+  position: absolute;
+  display: flex;
+  width: 240px;
+  height: 440px;
+  background: rgba(0, 0, 0, 0.7);
+  color: white;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 6px;
+`
+
+const Title = styled.div`
+  margin: 50px auto 30px 20px;
+  font-weight: bold;
+  font-size: 17px;
+`
+
+const Overview = styled.div`
+  margin: 0 20px;
+  line-height: 1.4;
+  font-size: 17px;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 10;
+  -webkit-box-orient: vertical;
+`
+
