18554416e9ec571670a69f851e70f6cadba7d365
zzzzzuuuuu <agu0830@naver.com>
2024-04-28 14:27:55 +0900

    feat: Navbar, Footer UI 구현

--- 조이-안지유/movie/src/App.jsx
+++ 조이-안지유/movie/src/App.jsx
@@ -1,10 +1,14 @@
 import Main from "./pages/Main.jsx";
 import {movies} from "./assets/movies.js";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <>
+      <Navbar/>
       <Main data={movies}/>
+      <Footer/>
     </>
   )
 }
