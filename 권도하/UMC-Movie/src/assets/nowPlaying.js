44a87f3db4ec34013ac8fa54ca14454ff14d6d47
kwondoha <ehehgk@icloud.com>
2024-05-02 02:54:39 +0900

    토큰을 환경변수로

--- 권도하/UMC-Movie/src/assets/nowPlaying.js
+++ 권도하/UMC-Movie/src/assets/nowPlaying.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 const options = {
   method: 'GET',
   url: 'https://api.themoviedb.org/3/movie/now_playing',
-  params: {language: 'ko', page: '1'},
+  params: { language: 'ko', page: '1' },
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MWUwYWU5YjczMmQ5NmIxNmE0NTkyNjE4NzQzNDc4OCIsInN1YiI6IjY2MzFlY2U2YWQ1OWI1MDEyYjZjYTEzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jt-7nM-82bQY3V5PX-re8U2MWWA0XnJ51tBVTlcr1jQ'
+    Authorization: `Bearer ${process.env.REACT_APP_MOVIE_DB_TOKEN}`
   }
 };
 
