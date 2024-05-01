cc95499ce8994bff850e278be1b89f1167e52e7a
Danjjinbibbang <hyjmex@gmail.com>
2024-05-01 19:29:24 +0900

    movie-poster

--- 한단비/movie/movie-react/src/App.js
+++ 한단비/movie/movie-react/src/App.js
@@ -1,27 +1,66 @@
 import './App.css';
-import { movies } from './movieDummy';
 import Movie from './components/Movie';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MainPage from './pages/MainPage';
+import NowPlayingPage from './pages/NowPlayingPage';
+import PopularPage from './pages/PopularPage';
+import TopRatePage from './pages/TopRatePage';
+import UpComing from './pages/UpComing';
+import Navbar from './components/Navbar';
+import Footbar from './components/Footbar';
+import MovieDetailPage from './pages/MovieDetailPage'
 
 // movie components를 받는 container
 function App() {
+  // const [movies, setMovies] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('https://api.themoviedb.org/3/discover/movie?api_key=8ef789919cffed29e0e0fac9a90d79bc')
+  //     .then(response => response.json())
+  //     .then(data => setMovies(data))
+  //     .catch(error => console.error('API 호출 중 오류 발생:', error));
+  // }, []);
+
   return (
-    <div>
-      <div className='body-container'>
-        {
-        movies.results.map((item)=>{
-            return (
-              <Movie
-                key={item.id}
-                title = {item.title}
-                poster_path= {item.poster_path}
-                vote_average={item.vote_average}
-                overview={item.overview}
-                />
-            );
-        })
-      }
-      </div>
+    <div className='root-wrap'>
+      <BrowserRouter>
+        {/* routing에 영향을 받지 않고, 고정되고 싶은 것은 <Routes> 밖에 있어야 함! */}
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/nowplaying" element={<NowPlayingPage />} />
+          <Route path="/:title" element={<MovieDetailPage/>}/>
+          <Route path="/popular" element={<PopularPage />} />
+          <Route path="/toprated" element={<TopRatePage />} />
+          <Route path="/upcoming" element={<UpComing />} />
+        </Routes>
+        <Footbar />
+      </BrowserRouter>
     </div>
+
+    // <div>
+    //   {movies.results ? (
+    //     <div className='body-container'>
+    //     {
+    //     movies.results.map((item)=>{
+    //         return (
+    //           <Movie
+    //             key={item.id}
+    //             title = {item.title}
+    //             poster_path= {item.poster_path}
+    //             vote_average={item.vote_average}
+    //             overview={item.overview}
+    //             />
+    //         );
+    //     })
+    //   }
+    //   </div>
+    //   ) : (
+    //     <p>영화 로딩 중...</p>
+    //   )}
+
+    // </div>
   );
 }
 
---
+++ 한단비/movie/movie-react/src/components/Banner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <div className='banner-container'>
+        <div className='banner-text'>환영합니다</div>
+    </div>
+  )
+}
