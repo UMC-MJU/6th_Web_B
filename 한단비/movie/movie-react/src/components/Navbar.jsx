ca12a17bc2984b5b8f0feb15b5e71a55f21590b2
Danjjinbibbang <hyjmex@gmail.com>
2024-05-07 01:12:58 +0900

    4주차 과제 및 Footbar 삭제

--- 한단비/movie/movie-react/src/components/Navbar.jsx
+++ 한단비/movie/movie-react/src/components/Navbar.jsx
@@ -1,31 +1,80 @@
-import React from 'react'
-import {Link} from'react-router-dom';
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavContainer = styled.div`
+  background-color: #171a32;
+`;
+
+const NavWrapper = styled.div`
+  height: 64px;
+  margin: 0;
+  justify-content: space-between;
+  display: flex;
+  align-items: center;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+
+  > ul {
+    display: flex;
+  }
+
+  > ul > li {
+    margin-right: 14px;
+    padding: 8px;
+  }
+`;
+
+const LogoWrapper = styled.div`
+  margin-left: 20px;
+`;
+
+const StyledLink = styled(Link)`
+  color:  ${(props) => props.color || "white"};
+  text-decoration: none;
+`;
 
 export default function Navbar() {
+    const [logState, setLogState] = useState('로그인');
+
+    // 로그인 / 로그아웃 텍스트 변경
+    const toggleLog = () => {
+        if(logState === '로그인'){
+            setLogState('로그아웃');
+        } else {
+            setLogState('로그인');
+        }
+    }
   return (
-    <div className="nav-container">
-        <div className="nav-wrapper">
-            <Link className="logo" to='/popular'>UMC Movie</Link>
-            <div className='nav-right-wrap'>
-                <ul>
-                    <li>
-                        <Link className="nav-item" to='/'>회원가입</Link>
-                    </li>
-                    <li>
-                        <Link className="nav-item" to='/popular'>Popular</Link>
-                    </li>
-                    <li>
-                        <Link className="nav-item" to='/nowplaying'>Now Playing</Link>
-                    </li>
-                    <li>
-                        <Link className="nav-item" to='/toprated'>Top Rated</Link>
-                    </li>
-                    <li>
-                        <Link className="nav-item" to='/upcoming'>Upcoming</Link>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-  )
+    <NavContainer>
+      <NavWrapper>
+        <LogoWrapper>
+          <Link className="logo" to="/popular">
+            UMC Movie
+          </Link>
+        </LogoWrapper>
+        <ButtonWrapper>
+          <ul>
+            <li>
+              <StyledLink to="/" color='#E1C35C' onClick={toggleLog}>{logState}</StyledLink>
+            </li>
+            <li>
+              <StyledLink to="/popular">Popular</StyledLink>
+            </li>
+            <li>
+              <StyledLink to="/nowplaying">Now Playing</StyledLink>
+            </li>
+            <li>
+              <StyledLink to="/toprated">Top Rated</StyledLink>
+            </li>
+            <li>
+              <StyledLink to="/upcoming">Upcoming</StyledLink>
+            </li>
+          </ul>
+        </ButtonWrapper>
+      </NavWrapper>
+    </NavContainer>
+  );
 }
---
+++ 한단비/movie/movie-react/src/pages/NotFoundPage.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  DetailContainer,
+  DetailWrapper,
+  DetailInfo,
+  DetailTitle,
+} from "./MovieDetailPage";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const MainButton = styled(Link)`
+  color: white;
+  font-size: 30px;
+  text-decoration: none;
+`;
+const NotFoundPage = () => {
+  return (
+    <DetailContainer>
+      <DetailWrapper>
+        <DetailInfo alignItems="center">
+          <DetailTitle size="40px">Oops!</DetailTitle>
+          <DetailTitle size="20px">
+            예상치 못한 에러가 발생했습니다;
+          </DetailTitle>
+          <DetailTitle size="20px" style={{ fontStyle: "italic" }}>
+            Not Found
+          </DetailTitle>
+          <MainButton to="/">메인으로 이동하기</MainButton>
+        </DetailInfo>
+      </DetailWrapper>
+    </DetailContainer>
+  );
+};
+
+export default NotFoundPage;
