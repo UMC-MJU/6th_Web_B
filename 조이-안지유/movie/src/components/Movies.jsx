a769a33ef97174f9f1e3a822b4ee522d85b69766
zzzzzuuuuu <agu0830@naver.com>
2024-05-14 19:45:40 +0900

    feat: 영화 리스트에서 특정 영화 클릭시 해당 영화의 상세페이지로 이동

--- 조이-안지유/movie/src/components/Movies.jsx
+++ 조이-안지유/movie/src/components/Movies.jsx
@@ -2,10 +2,12 @@ import React, {useState} from 'react';
 import "../styles/movies.css";
 import MovieInfo from "./MovieInfo.jsx";
 import styled from "styled-components";
+import {useNavigate} from "react-router-dom";
 
 const Movies = (props) => {
   const {original_title, overview, poster_path, vote_average} = props.data;
   const [isHover, setIsHover] = useState(false);
+  const navigate = useNavigate();
   const openInfo = () => {
     setIsHover(true);
   }
@@ -14,7 +16,9 @@ const Movies = (props) => {
   }
 
   return (
-    <Movie onMouseEnter={openInfo} onMouseLeave={closeInfo}>
+    <Movie onMouseEnter={openInfo} onMouseLeave={closeInfo} onClick={() => {
+      navigate(`/movie/${original_title}`)
+    }}>
       {isHover && <MovieInfo title={original_title} info={overview}/>}
       <MoviePoster src={`https://image.tmdb.org/t/p/w500/${poster_path}`}/>
       <MovieBasic>
