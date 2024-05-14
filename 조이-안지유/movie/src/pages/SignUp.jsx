620e2e25ecf42bd60994a356ed4821b67d96fe27
zzzzzuuuuu <agu0830@naver.com>
2024-05-14 17:27:27 +0900

    feat: 회원가입 유효성검사

--- 조이-안지유/movie/src/pages/SignUp.jsx
+++ 조이-안지유/movie/src/pages/SignUp.jsx
@@ -2,54 +2,66 @@ import React from 'react';
 import styled from "styled-components";
 import SignUpInput from "../components/SignUpInput.jsx";
 import {useForm} from "react-hook-form";
+import {useNavigate} from "react-router-dom";
 
 const SignUp = () => {
   const {
     register,
     handleSubmit,
     watch,
-    setError,
     formState: {errors},
   } = useForm({
     mode: "onChange" // 입력 필드가 변경될 때마다 유효성검사 실행
   });
-  const checkPassword = watch("password");
+  const password = watch("password");
   const onSubmit = (data) => {
-    if (data.password !== data.checkPassword) {
-      setError("checkPassword", {
-        type: "manual",
-        message: "비밀번호가 일치하지 않습니다.",
-      });
-      return;
-    }
     console.log(data);
   }
+
   return (
     <SignUpContainer>
       <Title>회원가입 페이지</Title>
       <SignUpForm onSubmit={handleSubmit(onSubmit)}>
-        <SignUpInput name="name" type="text" placeholder="이름을 입력해주세요" {...register("name", {required: "이름을 입력해주세요!"})}
-                     errors={errors}/>
-        <SignUpInput name="email" type="email"
-                     placeholder="이메일을 입력해주세요" {...register("email", {required: "이메일을 입력해주세요!"})} errors={errors}/>
-        <SignUpInput name="age" type="text" placeholder="나이를 입력해주세요" {...register("age", {
-          required: "나이를 입력해주세요!",
-          min: {value: 19, message: "19세 이상만 이용가능합니다!"},
-          pattern: {value: /^\d+$/, message: "나이는 숫자로 입력해주세요!"}
+        <Input name="name" type="text" placeholder="이름을 입력해주세요" {...register("name", {
+          required: "이름을 입력해주세요!"
         })} errors={errors}/>
-        <SignUpInput name="password" type="password"
-                     placeholder="비밀번호를 입력해주세요" {...register("password", {
-          required: "비밀번호를 입력해주세요!",
-          minLength: {value: 4, message: "최소 4자리 이상 입력해주세요."},
-          maxLength: {value: 12, message: "최대 12자리까지만 입력이 가능합니다."},
+        {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
+
+        <Input name="email" type="email"
+               placeholder="이메일을 입력해주세요" {...register("email", {
+          required: "이메일을 입력해주세요!",
           pattern: {
-            value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{4,12}$/,
-            message: "비밀번호는 영어, 숫자, 특수문자를 모두 포함해주세요."
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+            message: "이메일 형식에 맞게 입력해주세요!"
           }
         })} errors={errors}/>
-        <SignUpInput name="checkPassword" type="password"
-                     placeholder="비밀번호 확인" {...register("checkPassword", {required: "비밀번호를 다시 입력해주세요!"})}
-                     errors={errors}/>
+        {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+
+        <Input name="age" type="text" placeholder="나이를 입력해주세요" {...register("age", {
+          required: "나이를 입력해주세요!",
+          validate: {
+            positive: v => parseInt(v) > 0 || "나이는 양수여야 합니다.",
+            isInteger: v => parseInt(v) === parseFloat(v) || "나이는 실수로 입력할 수 없습니다.",
+            minAge: v => parseInt(v) >= 19 || "19세 이상만 이용가능합니다!"
+          }
+        })} errors={errors}/>
+        {errors.age && <ErrorMessage>{errors.age.message}</ErrorMessage>}
+
+        <Input name="password" type="password" placeholder="비밀번호를 입력해주세요" {...register("password", {
+          required: "비밀번호를 입력해주세요!",
+          validate: {
+            minPw: v => String(v).length >= 4 || "최소 4자리 이상 입력해주세요.",
+            maxPw: v => String(v).length <= 12 || "최대 12자리까지만 입력이 가능합니다.",
+            pattern: v => /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(v) || "비밀번호는 영어, 숫자, 특수문자를 모두 포함해주세요.",
+          },
+        })} errors={errors}/>
+        {errors.password && <ErrorMessage>{errors.password.message}</ErrorMessage>}
+
+        <Input name="checkPassword" type="password" placeholder="비밀번호를 다시 입력해주세요" {...register("checkPassword", {
+          required: "비밀번호를 다시 입력해주세요!",
+          validate: {samePw: v => v === password || "비밀번호가 일치하지 않습니다.",}
+        })} errors={errors}/>
+        {errors.checkPassword && <ErrorMessage>{errors.checkPassword.message}</ErrorMessage>}
         <SubmitBtn type="submit">제출하기</SubmitBtn>
       </SignUpForm>
       <AdditionalBox>
@@ -82,6 +94,20 @@ const SignUpForm = styled.form`
   flex-direction: column;
   gap: 15px;
 `
+
+const Input = styled.input`
+  width: 30vw;
+  height: 35px;
+  border-radius: 30px;
+`
+
+const ErrorMessage = styled.p`
+  color: red;
+  font-size: 12px;
+  //margin: 0;
+  margin: 5px 0 0 10px;
+`
+
 
 const SubmitBtn = styled.button`
   width: 30vw;
