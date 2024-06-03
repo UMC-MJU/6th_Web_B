9dc22e96978828e54f29338852554a7b7953f057
zzzzzuuuuu <agu0830@naver.com>
2024-06-03 17:21:12 +0900

    feat: Popular 페이지 pagination 구현

--- 조이-안지유/movie/src/pages/PopularPage.jsx
+++ 조이-안지유/movie/src/pages/PopularPage.jsx
@@ -2,15 +2,18 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import styled from "styled-components";
 import Movies from "../components/Movies.jsx";
+import Pagination from "../components/Pagination.jsx";
 
 const PopularPage = () => {
   const [movieData, setMovieData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
-  useEffect(() => {
+  const fetchMovies = (page) => {
     const options = {
       method: 'GET',
       url: 'https://api.themoviedb.org/3/movie/popular',
-      params: {language: 'ko-KR'},
+      params: {language: 'ko-KR', page},
       headers: {
         accept: 'application/json',
         Authorization: 'Bearer ' + import.meta.env.VITE_APP_KEY
@@ -19,19 +22,38 @@ const PopularPage = () => {
     axios
       .request(options)
       .then(function (response) {
-        console.log(response.data);
+        console.log(response);
         setMovieData(response.data.results)
+        setTotalPages(response.data.total_pages);
       })
       .catch(function (error) {
         console.error(error);
       });
-  }, [])
+  }
+  console.log(totalPages);
+
+  useEffect(() => {
+    fetchMovies(currentPage);
+  }, [currentPage]);
+
+  const onPageChange = (page) => {
+    if (page > 0 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+    console.log("페이지 이동!");
+  };
+
+
   return (
-    <MovieContainer>
-      {movieData.map((data, index) => (
-        <Movies data={data} key={index}/>
-      ))}
-    </MovieContainer>
+    <PopularContainer>
+      <MovieContainer>
+        {movieData.map((data, index) => (
+          <Movies data={data} key={index}/>
+        ))}
+      </MovieContainer>
+      <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={onPageChange}/>
+    </PopularContainer>
+
   );
 };
 
@@ -45,4 +67,10 @@ const MovieContainer = styled.div`
   width: 100%;
   height: auto;
   flex-wrap: wrap;
+  border: none;
+`
+
+const PopularContainer = styled.div`
+  display: flex;
+  flex-direction: column;
 `
