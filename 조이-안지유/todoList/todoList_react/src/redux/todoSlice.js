5b22a02c17b759e15290fd0ba3e9e55ea8276509
zzzzzuuuuu <agu0830@naver.com>
2024-07-01 15:39:40 +0900

    refactor: 리듀서 함수명 수정

--- 조이-안지유/todoList/todoList_react/src/redux/todoSlice.js
+++ 조이-안지유/todoList/todoList_react/src/redux/todoSlice.js
@@ -1,21 +1,19 @@
 import {createSlice} from "@reduxjs/toolkit";
 
-let nextId = 0;
 const initialState = [];
 
 export const todoSlice = createSlice({
   name: 'todoFunction',
   initialState,
   reducers: {
-    add: (state, action) => { // 할 일 입력창에 입력되는 텍스트 값
-      nextId++;
+    addTodo: (state, action) => { // 할 일 입력창에 입력되는 텍스트 값
       state.push({
-        id: nextId,
+        id: Date.now(),
         text: action.payload,
         complete: false,
       });
     },
-    remove: (state, action) => { // 선택된 할 일을 제외한 모든 할 일을 새로운 객체로 리턴
+    deleteTodo: (state, action) => { // 선택된 할 일을 제외한 모든 할 일을 새로운 객체로 리턴
       return state.filter((e) => e.id !== action.payload);
     },
     complete: (state, action) => { // 선택된 할 일을 true 값으로 변경
@@ -23,5 +21,9 @@ export const todoSlice = createSlice({
     }
   }
 });
-export const {add, remove, complete} = todoSlice.actions;
+
+// reducer를 전역에서 사용하기 위해 slice 이름 뒤에 .actions를 붙여 export
+export const {addTodo, deleteTodo, complete} = todoSlice.actions;
 export default todoSlice.reducer;
+
+console.log(todoSlice.actions);
