10e8a0213d3b5a605e06a3a875e42d4889f9d918
zzzzzuuuuu <agu0830@naver.com>
2024-07-02 16:40:55 +0900

    feat: 영화 검색시 결과 div창이 input 밑에 뜨도록

--- 조이-안지유/movie/src/pages/MainPage.jsx
+++ 조이-안지유/movie/src/pages/MainPage.jsx
@@ -10,9 +10,9 @@ const MainPage = () => {
       </BannerContainer>
       <SearchContainer>
         <SearchText>📽 Find your movies !</SearchText>
-        {/*<SearchBox>*/}
-        <SearchMovies/>
-        {/*</SearchBox>*/}
+        <SearchBox>
+          <SearchMovies/>
+        </SearchBox>
       </SearchContainer>
     </MainContainer>
   );
@@ -23,7 +23,7 @@ export default MainPage;
 const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  height: 150vh;
 `
 
 const BannerContainer = styled.div`
@@ -45,16 +45,39 @@ const TitleText = styled.h1`
   @media (max-height: 400px) {
     font-size: 3vh;
   }
-
 `
 
-const SearchContainer = styled(BannerContainer)`
+const SearchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  left: 0;
+  width: 100%;
+  height: 100vh;
   background: rgb(26, 35, 78);
   flex-direction: column;
-  justify-content: center;
+  position: relative; // 기준
 `
 
-const SearchText = styled(TitleText)`
+const SearchText = styled.h1`
+  position: absolute; // relative에 따라 ...
+  top: 10%;
+  color: white;
+  text-align: center;
+  max-width: 100%;
+  max-height: 100%;
+  font-size: 4vw;
   margin: 0;
   padding-bottom: 18vh;
+
+  @media (max-height: 400px) {
+    font-size: 3vh;
+  }
+
+`
+const SearchBox = styled.div`
+  position: absolute;
+  top: 22%;
+  display: flex;
+  justify-content: center;
 `
